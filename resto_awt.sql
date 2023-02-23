202f2ed96d6e7a787f0b43e07c78ce7475821ea2
Eliefrh <2067396@bdeb.qc.ca>
2023-02-23 14:59:00 -0500

    nouvelle script de creation base de donnees

--- resto_awt.sql
+++ resto_awt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 09, 2023 at 08:17 PM
+-- Generation Time: Feb 23, 2023 at 07:58 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -24,29 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categorie`
---
-
-DROP TABLE IF EXISTS `categorie`;
-CREATE TABLE IF NOT EXISTS `categorie` (
-  `cat_id` int NOT NULL,
-  `cat_nom` varchar(30) NOT NULL,
-  `cat_description` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`cat_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `categorie`
---
-
-INSERT INTO `categorie` (`cat_id`, `cat_nom`, `cat_description`) VALUES
-(1, 'pizzas', 'Les meilleurs pizzas fait maison à montréal '),
-(2, 'poutines', 'Des vrais poutines québecois'),
-(3, 'desserts', 'Ca vous prend un bon dessert aprés vos repas chez nous ');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `client`
 --
 
@@ -61,43 +38,37 @@ CREATE TABLE IF NOT EXISTS `client` (
   `cl_address` varchar(200) NOT NULL,
   `cl_password` varchar(50) NOT NULL,
   `resto_id` int NOT NULL,
-  PRIMARY KEY (`cl_id`)
+  `reservation_id` int DEFAULT NULL,
+  PRIMARY KEY (`cl_id`),
+  UNIQUE KEY `reservation_id` (`reservation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `client`
 --
 
-INSERT INTO `client` (`cl_id`, `cl_nom`, `cl_prenom`, `cl_telephone`, `cl_courriel`, `cl_code_postal`, `cl_address`, `cl_password`, `resto_id`) VALUES
-(31569, 'singh', 'gaurav', '4384561324', '2215536@bdeb.qc.ca', 'h1ll1h', '10500 boul de l\'acadie, montreal', '1111', 2645),
-(56513, 'alfrieh', 'elie', '5144155145', '2067396@bdeb.qc.ca', 'h4nn4h', '10500 bois de boulogne , montreal', '0000', 2645),
-(64587, 'hua', 'anthony', '5144567894', '2183423@bdeb.qc.ca', 'h1zz1h', '1500 cote vertu, montreal', '2222', 2645);
+INSERT INTO `client` (`cl_id`, `cl_nom`, `cl_prenom`, `cl_telephone`, `cl_courriel`, `cl_code_postal`, `cl_address`, `cl_password`, `resto_id`, `reservation_id`) VALUES
+(31569, 'singh', 'gaurav', '4384561324', '2215536@bdeb.qc.ca', 'h1ll1h', '10500 boul de l\'acadie, montreal', '1111', 2645, NULL),
+(56513, 'alfrieh', 'elie', '5144155145', '2067396@bdeb.qc.ca', 'h4nn4h', '10500 bois de boulogne , montreal', '0000', 2645, NULL),
+(64587, 'hua', 'anthony', '5144567894', '2183423@bdeb.qc.ca', 'h1zz1h', '1500 cote vertu, montreal', '2222', 2645, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `facture`
+-- Table structure for table `commande`
 --
 
-DROP TABLE IF EXISTS `facture`;
-CREATE TABLE IF NOT EXISTS `facture` (
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
   `fac_id` int NOT NULL,
   `fac_total` float NOT NULL,
-  `pan_id` int NOT NULL,
   `cl_id` int NOT NULL,
-  `date_facturer` datetime NOT NULL,
+  `date_commande` datetime NOT NULL,
+  `it_id` int NOT NULL,
   PRIMARY KEY (`fac_id`),
-  UNIQUE KEY `pan_id` (`pan_id`),
-  UNIQUE KEY `cl_id` (`cl_id`)
+  UNIQUE KEY `cl_id` (`cl_id`),
+  UNIQUE KEY `it_id` (`it_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `facture`
---
-
-INSERT INTO `facture` (`fac_id`, `fac_total`, `pan_id`, `cl_id`, `date_facturer`) VALUES
-(41127, 50.65, 2, 31569, '2023-02-09 19:38:01'),
-(65425, 44.17, 1, 56513, '2023-02-09 19:38:01');
 
 -- --------------------------------------------------------
 
@@ -117,20 +88,6 @@ CREATE TABLE IF NOT EXISTS `items` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `panier`
---
-
-DROP TABLE IF EXISTS `panier`;
-CREATE TABLE IF NOT EXISTS `panier` (
-  `pan_id` int NOT NULL,
-  `it_id` int NOT NULL,
-  PRIMARY KEY (`pan_id`),
-  UNIQUE KEY `it_id` (`it_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `produits`
 --
 
@@ -141,18 +98,20 @@ CREATE TABLE IF NOT EXISTS `produits` (
   `prod_ingredient` varchar(200) NOT NULL,
   `prod_prix` float NOT NULL,
   `prod_calories` int NOT NULL,
-  `prod_img` varchar(200) NOT NULL,
-  `cat_id` int NOT NULL,
-  PRIMARY KEY (`prod_id`),
-  UNIQUE KEY `cat_id` (`cat_id`)
+  `cat_nom` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`prod_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `produits`
 --
 
-INSERT INTO `produits` (`prod_id`, `prod_nom`, `prod_ingredient`, `prod_prix`, `prod_calories`, `prod_img`, `cat_id`) VALUES
-(6512, 'lava pizza', 'Pizza toute garnie avec spaghetti et sauce à la viande', 18, 500, 'https://cdn.restomenu.com/web/img/section/6134/7ce5f86b70877e3bd89737b028ce57a9.jpg', 1);
+INSERT INTO `produits` (`prod_id`, `prod_nom`, `prod_ingredient`, `prod_prix`, `prod_calories`, `cat_nom`) VALUES
+(3156, 'pizza fromage', 'fromage', 15.75, 450, 'pizza'),
+(6512, 'lava pizza', 'Pizza toute garnie avec spaghetti et sauce à la viande', 18, 500, 'pizza'),
+(7413, 'pizza poulet buffalo', 'Poulet grillé, sauce buffalo, fromage mozzarella et dressing au fromage bleu', 19.25, 545, 'pizza'),
+(9456, 'pizza aux champignons', 'Champignons grillés, fromage mozzarella et sauce tomate', 16.75, 435, 'pizza'),
+(85752, 'hawaiian pizza', 'Jambon, ananas, fromage mozzarella et sauce tomate', 17.5, 550, 'pizza');
 
 -- --------------------------------------------------------
 
@@ -163,22 +122,27 @@ INSERT INTO `produits` (`prod_id`, `prod_nom`, `prod_ingredient`, `prod_prix`, `
 DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE IF NOT EXISTS `reservation` (
   `reservation_id` int NOT NULL,
-  `reserver_id` int NOT NULL,
-  `num_siege` int NOT NULL
+  `num_siege` int NOT NULL,
+  `cl_id` int NOT NULL,
+  `date_reservation` datetime NOT NULL,
+  PRIMARY KEY (`reservation_id`),
+  UNIQUE KEY `cl_id` (`cl_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reserver`
+-- Table structure for table `review`
 --
 
-DROP TABLE IF EXISTS `reserver`;
-CREATE TABLE IF NOT EXISTS `reserver` (
-  `reserver_id` int NOT NULL,
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE IF NOT EXISTS `review` (
   `cl_id` int NOT NULL,
-  `num_siege_reserve` int NOT NULL,
-  `date_reserve` datetime NOT NULL
+  `review_id` int NOT NULL,
+  `etoiles` int NOT NULL,
+  `text` varchar(1000) NOT NULL,
+  PRIMARY KEY (`review_id`),
+  UNIQUE KEY `cl_id` (`cl_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -186,10 +150,11 @@ CREATE TABLE IF NOT EXISTS `reserver` (
 --
 
 --
--- Constraints for table `facture`
+-- Constraints for table `commande`
 --
-ALTER TABLE `facture`
-  ADD CONSTRAINT `facture_ibfk_1` FOREIGN KEY (`cl_id`) REFERENCES `client` (`cl_id`);
+ALTER TABLE `commande`
+  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`cl_id`) REFERENCES `client` (`cl_id`),
+  ADD CONSTRAINT `commande_ibfk_2` FOREIGN KEY (`it_id`) REFERENCES `items` (`it_id`);
 
 --
 -- Constraints for table `items`
@@ -198,17 +163,16 @@ ALTER TABLE `items`
   ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`prod_id`) REFERENCES `produits` (`prod_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `panier`
+-- Constraints for table `reservation`
 --
-ALTER TABLE `panier`
-  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`it_id`) REFERENCES `items` (`it_id`),
-  ADD CONSTRAINT `panier_ibfk_2` FOREIGN KEY (`pan_id`) REFERENCES `facture` (`pan_id`);
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `client` (`reservation_id`);
 
 --
--- Constraints for table `produits`
+-- Constraints for table `review`
 --
-ALTER TABLE `produits`
-  ADD CONSTRAINT `produits_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `categorie` (`cat_id`);
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`cl_id`) REFERENCES `client` (`cl_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
